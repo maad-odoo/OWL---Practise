3980a18a1a3899a0d4596e94d1077a28d5ca7914
maad-odoo <maad@odoo.com>
2023-02-10 12:30:10 +0530

    implemented basic useRef

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-const { Component, xml, useState , mount } = owl;
-
-class Counter extends Component {
-  static template = xml`
-    <button t-on-click="increment">
-      Click Me! [<t t-esc="props.name.value"/>]    
-    </button>`;     // recieved the value from state and directly displayed it
-
-    increment() {
-        this.props.name.value++;                          // used the age method and executed it
-        this.props.name.age = () => {                     // you can directly use the method as it is
-            console.log("from counter");                  // or you can override them and the then call them
-        }
-       this.props.name.age()
-      }
-    static props = [
-        "name"
-      ]
-}
-
+const { Component, xml, useRef , mount } = owl;
 class Root extends Component{
     static template =xml`
-    <Counter name="name"/>`
-    
-    name = useState({ value: 10 , age: function(){   //passing a variable 'value' and a method age into a state
-        console.log("from root");
-        return 23;
-    } });
+    <div>
+        <input t-ref="1"/>
+        <span>hello</span>
+        <button t-on-click="btn">click</button>
+    </div>`
 
-    static components = {Counter}
+    ref1 = useRef("1");
+
+    btn(){
+        console.log(this.ref1.el.value);
+    }
+   
 }
 
 mount(Root, document.body);
